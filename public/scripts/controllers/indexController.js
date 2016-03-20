83306ec117529b947094115757bdb5a9214b479b
Gwendolyn Williams <gwilliwaite@gmail.com>
2016-03-20 12:40:30 -0500

    styling

--- public/scripts/controllers/indexController.js
+++ public/scripts/controllers/indexController.js
@@ -1,10 +1,14 @@
 myApp.controller('IndexController', ['$scope', '$http', '$window', function($scope, $http, $window) {
 
+    $scope.registered = false;
+
     $scope.logout = function() {
 
         $http.get('/logout').then(function(response) {
             $scope.loggedOut = 'You have successfully logged out';
         });
     };
+
+
 
 }]);
